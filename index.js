5f77e0337ad36dc14b411c1db439a3b5636e1b2d
clairem <claire.monnier@outlook.com>
2019-11-30 18:04:52 +0100

    transfert feu tricolore v1

---
+++ index.js
@@ -0,0 +1,53 @@
+
+let cercleBas = document.querySelector('#cercle');
+let cercleCentre = document.querySelector('#cercleCentre');
+let cercleHaut = document.querySelector('#cercleHaut');
+let button = document.querySelector('button');
+ 
+let change;
+let secondChange;
+
+//Feu du bas passe au gris et feu du milieu passe au orange 
+function greenToGrey() {
+    cercleBas.style.background = "grey";
+}
+
+function greyToOrange() { 
+    cercleCentre.style.background = "orange";
+} 
+
+//Feu du milieu passe au gris et feu du haut passe au rouge
+function delay() {
+    change = setTimeout(orangeToRed,2000);
+}
+
+
+function orangeToRed() {
+    cercleHaut.style.background = "red";
+    cercleCentre.style.background = "grey";
+}
+
+//Feu du haut passe au gris et feu du bas repasse au vert
+function secondDelay() {
+    secondChange = setTimeout(backToGreen,5000);
+}
+
+function backToGreen() {
+    cercleHaut.style.background = "grey";
+    cercleBas.style.background = "green";
+}
+
+
+function execute() {
+            greenToGrey();
+            greyToOrange();
+            delay();
+            secondDelay();                 
+}
+
+
+function repeat() {
+    setInterval(execute, 8000);
+    
+}
+
